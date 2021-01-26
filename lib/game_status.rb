e6bf98d1f373890813a3e911585a73ccfa5087be
Annie Wiegel <annewiegel17@gmail.com>
2021-01-26 02:45:24 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,10 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2], #Top row
+  [3,4,5],  #Middle row
+  [6,7,8],   #Bottom row
+  [0,4,8], #Diagonal TL-BR
+  [6,4,2] #Diagonal BL-TR
+]
