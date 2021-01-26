3368a2fc0ad938cfac8b17b557e266ad548c3f3d
Annie Wiegel <annewiegel17@gmail.com>
2021-01-26 04:29:22 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -16,7 +16,7 @@ WIN_COMBINATIONS = [
 ]
 
 def won?(board)
-  WIN_COMBINATIONS.each do |win_combo|
+  WIN_COMBINATIONS.each {|win_combo|
       index_0 = win_combo[0]
       index_1 = win_combo[1]
       index_2 = win_combo[2]
@@ -29,7 +29,8 @@ def won?(board)
            return win_combo
      elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
           return win_combo
-           return false
-      end
+        end
+      }
+  return false
     end
 end
