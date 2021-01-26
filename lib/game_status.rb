fe358af5d10326cd3fddb747a5d466e36fde346a
Annie Wiegel <annewiegel17@gmail.com>
2021-01-26 03:30:01 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -18,9 +18,5 @@ WIN_COMBINATIONS = [
 def won?(board)
   WIN_COMBINATIONS.any? do |winner|
     winner == ["X", "X", "X"] || winner == ["O", "O", "O"]
-
-
-  WIN_COMBINATIONS.select do |winner|
-    winner == ["X", "X", "X"] || winner == ["O", "O", "O"]
-    end
+  end
 end
